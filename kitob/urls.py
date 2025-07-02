5f32a18d1fe60d145b803acd3d1ce79bde77ebef
xojanazarovasilbek <xojanazarovasilbek1908@gmail.com>
2025-07-02 21:54:48 +0500

    tayyor

--- kitob/urls.py
+++ kitob/urls.py
@@ -3,5 +3,5 @@ from .views import book_list, book_detail
 
 urlpatterns = [
     path('', book_list, name='kitob'),
-    path('<int:pk>/', book_detail, name='book_detail'),  
+    path('<int:pk>/', book_detail, name='bookl'),
 ]
